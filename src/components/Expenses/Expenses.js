0808ab63c1fdbe460acc6ab6c57c8b0c13fecdfc
Jeremiah Metzen <jmetzen@colinsgrp.com>
2022-09-28 16:39:00 -0500

    My first pass at the assignment.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,33 @@
+import {useState} from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../UI/ExpensesFilter";
 
 function Expenses(props){
 
+    // Filter from filter up to Expenses (this) component.
+    // Store filter data in state here.
+
+    const [filterData, setFilterData] = useState("");
+
+    const onFilterChange = (changedData) => {
+        console.log(changedData);
+        setFilterData(changedData);
+    };
+
     return(
-        <div>
-            <ExpenseFilter />
 
             <Card className="expenses">
+                <div><ExpensesFilter onFilterChange={onFilterChange} /></div>
                 {
                     props.expenses.map(expense => {
                         return <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
                     })
                 }
             </Card>
-        </div>
+    
     );
 }
 
